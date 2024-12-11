db684db23f16dba38322b229062ba7460c8b2246
Kolesnikov Ilya <ilja-kolesnikov@mail.ru>
2024-12-11 14:33:18 +0300

    Added main screen with 3 flags-buttons

--- Guess The Flag/ViewController.swift
+++ Guess The Flag/ViewController.swift
@@ -8,10 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var button1: UIButton!
+    
+    @IBOutlet var button2: UIButton!
+    
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+
+        askQuestion()
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
     }
 
 
